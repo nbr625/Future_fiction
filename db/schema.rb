8ed0f44ede299f41b765d14df7e16af465269ee3
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-02-11 17:38:51 -0500

    Added paperclip for Book images

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211201752) do
+ActiveRecord::Schema.define(version: 20150211221434) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
     t.text     "description"
     t.string   "year_published"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
